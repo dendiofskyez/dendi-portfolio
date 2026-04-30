81dac54d11d0fd875c3d90a33aa418a695b60f7f
dendiofskyez <dendipermana1107@gmail.com>
2026-05-01 05:51:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { motion } from 'framer-motion';
 
 export default function Portfolio() {
